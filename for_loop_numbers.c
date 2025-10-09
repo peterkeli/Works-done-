f1ef9766e8dd5b0fe81809eefa456ce2025622c9
peterkeli <peterkeli896@gmail.com>
2025-10-09 22:20:50 +0300

    Add files via upload

---
+++ for_loop_numbers.c
@@ -0,0 +1,17 @@
+/* 
+name: Mathias kioko 
+registration no: CT100/G/26229/25
+Description: C program  using a for_loop.
+*/
+
+#include  <stdio.h>
+int main (){
+
+   int i;
+   for(i=100;i>=50;i--)
+   {
+   printf("%d\n",i);
+   }
+
+return 0;
+}
